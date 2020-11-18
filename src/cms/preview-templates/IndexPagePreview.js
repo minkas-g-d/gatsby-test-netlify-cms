bcd1a14b95a5a95c4b195710568982f271766709
minkas-g-d <mina.dodunekova@gmail.com>
2020-11-18 22:03:31 +0200

    Fix indentation

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -1,32 +1,33 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { IndexPageTemplate } from '../../templates/index-page'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { IndexPageTemplate } from '../../templates/index-page';
 
 const IndexPagePreview = ({ entry, getAsset }) => {
-  const data = entry.getIn(['data']).toJS()
-
-  if (data) {
-    return (
-      <IndexPageTemplate
-        image={getAsset(data.image)}
-        title={data.title}
-        heading={data.heading}
-        subheading={data.subheading}
-        description={data.description}
-        intro={data.intro || { blurbs: [] }}
-        mainpitch={data.mainpitch || {}}
-      />
-    )
-  } else {
-    return <div>Loading...</div>
-  }
-}
+	const data = entry.getIn(['data']).toJS();
+	console.log('entry', entry.getIn(['data']));
+	console.log('hello', entry.getIn(['data']));
+	if (data) {
+		return (
+			<IndexPageTemplate
+				image={getAsset(data.image)}
+				title={data.title}
+				heading={data.heading}
+				subheading={data.subheading}
+				description={data.description}
+				intro={data.intro || { blurbs: [] }}
+				mainpitch={data.mainpitch || {}}
+			/>
+		);
+	} else {
+		return <div>Loading...</div>;
+	}
+};
 
 IndexPagePreview.propTypes = {
-  entry: PropTypes.shape({
-    getIn: PropTypes.func,
-  }),
-  getAsset: PropTypes.func,
-}
+	entry: PropTypes.shape({
+		getIn: PropTypes.func,
+	}),
+	getAsset: PropTypes.func,
+};
 
-export default IndexPagePreview
+export default IndexPagePreview;
